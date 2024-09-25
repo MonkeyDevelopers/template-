ca280d601b4ee54bae997eda8287cf3435b83641
Caio Emidio <caio.emidio@gmail.com>
2024-09-25 17:28:51 +0100

    feat: input done with veevalidator carai!

--- pages/tasks.vue
+++ pages/tasks.vue
@@ -1,22 +1,12 @@
 <template>
   <nuxt-layout name="admin" title="Tarefas" description="Tarefas">
-    <UIForm @onSubmit="onSubmit" :validation-schema="schema">
-      <UIInput
-        name="name"
-        type="text"
-        label="Full Name"
-        placeholder="Your Name"
-        success-message="Nice to meet you!"
-      ></UIInput>
-      <KoButton color="primary" @click="save"> Save </KoButton>
-    </UIForm>
+    <TasksForm v-model="tasks" />
+    <br />
     <UIDataTable :headers :items="tasks"></UIDataTable>
   </nuxt-layout>
 </template>
 
 <script setup>
-import * as Yup from "yup";
-
 const headers = [
   {
     key: "id",
@@ -28,19 +18,6 @@ const headers = [
   },
 ];
 const tasks = ref();
-const task = ref("");
-
-function onSubmit(values) {
-  alert(JSON.stringify(values, null, 2));
-}
-
-const schema = Yup.object().shape({
-  name: Yup.string().required(),
-});
-
-const save = async () => {
-  tasks.value.push({ id: tasks.value.length + 1, title: task.value });
-};
 
 const fetchTasksFromServerRoute = async () => {
   const data = await $fetch("/api/tasks");
